a7523b6ccf950736afc2c49b429c42e9a239b64b
aqeelarshad811 <aqeelarshad811@gmail.com>
2024-12-03 19:57:17 +0500

    update register user

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -7,173 +7,192 @@ const jwt = require("jsonwebtoken");
 // module.exports.
 const registerUser = async (req, res) => {
     const { username, email, password } = req.body;
-try{
     if (!username || !email || !password) {
         return res.status(400).json({ message: "All fields are required", success: false });
     }
-    const existedUser = await User.findOne(
-        {
-            $or: [
-                {
-                    username
-                    
-                }, {
-                    email
-                }]
-        })
+    let user;
+    try {
+        const existedUser = await User.findOne(
+            {
+                $or: [
+                    {
+                        username
 
-    if (existedUser) {
-        return res.status(409).json({ message: "User already exists", success: false });
-    }
+                    }, {
+                        email
+                    }]
+            })
 
-    let role="user"
-    const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
-    console.log("verificatin code : ",verificationCode)
-    if(process.env.ADMIN_SECRET===req.body.secret){
-        role="admin";
-    }
-    const user = new User(
-        {
-            username,
-            email,
-            password,
-            verificationCode,
-            role
-        });
-        try {
-            console.log("secret of admin : ",req.body.secret)
-        //const verify = sendVerificationCode(user.email, verificationCode);
-        const verify =await sendEmail("aqeelarshad811@gmail.com","verify","aqeel811",verificationCode)
+            if (existedUser && existedUser.isVerified===false) {
+               const deletedUser = await User.findByIdAndDelete(existedUser._id);
+            }
+        if (existedUser && existedUser.isVerified === true) {
 
-        //const verify =await sendEmail(user.email,"verify",user.username,verificationCode)
-        console.log("verify user details : ",verify)
-        if (!verify) {
-            return res.status(500).json({ message: "Error while sending verification code && email account not exist ! ", success: false });
+            return res.status(409).json({ message: "User already exists", success: false });
         }
-        
-    }catch(error){
-        console.log("error in sending email : ",error)
-        return res.status(500).json({ message: "Error while sending verification code !", success: false });
-    }
-    const newUser = await user.save()
-    const userWithoutPassword = await User.findById(newUser._id).select("-password -refreshToken")
-    
-       return  res
-        .status(201)
-        .json({ message: "User registered successfully", data: userWithoutPassword, success: true })
+
+        let role = "user"
+        const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+        console.log("verificatin code : ", verificationCode)
+        if (process.env.ADMIN_SECRET === req.body.secret) {
+            role = "admin";
+        }
+         user = new User(
+            {
+                username,
+                email,
+                password,
+                verificationCode,
+                role
+            });
+        try {
+            console.log("secret of admin : ", req.body.secret)
+            // const verify =await sendEmail("aqeelarshad811@gmail.com","verify","aqeel811",verificationCode)
+
+            const verify = await sendEmail(user.email, "verify", user.username, verificationCode)
+            console.log("verify user details : ", verify)
+            if (!verify) {
+                return res.status(500).json({ message: "Error while sending verification code && email account not found ! ", success: false });
+            }
+
+        } catch (error) {
+            console.log("error in sending email : ", error)
+
+            return res.status(500).json({ message: "Error while sending verification code !", success: false });
+        }
+        const newUser = await user.save()
+        const userWithoutPassword = await User.findById(newUser._id).select("-password -refreshToken")
+
+        return res
+            .status(201)
+            .json({ message: "User registered successfully", data: userWithoutPassword, success: true })
 
     } catch (error) {
+        if (user && user._id) {
+            await User.findByIdAndDelete(user._id);
+        }
         console.log("error while registering user", error)
         res
             .status(500)
             .json({ message: "Error while registering user", error, success: false });
     }
-} 
+}
+// const otpSend=async(req,res)=>{
+//     try {
+
+//         const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+//         console.log("verificatin code : ",verificationCode)
+//     } catch (error) {
+//         console.log("error in otp sending : ",error)
+//         res.status(500).json({message:"Error while sending otp",error,success:false})
+//     }
+// }
 // module.exports.
 const verifyUser = async (req, res) => {
     try {
-        const{verificationCode}=req.body;
-        if(!verificationCode){
+        const { verificationCode } = req.body;
+        if (!verificationCode) {
             return res.status(400).json({ message: "Verification code is required", success: false });
         }
         const user = await User.findOne({ verificationCode });
         if (!user) {
             return res.status(404).json({ message: "Invalid verification code or User not found ", success: false });
         }
-        if(user?.isVerified === true){
+        if (user?.isVerified === true) {
             return res
-            .status(200)
-            .json({ message: "User already verified", success: false });
+                .status(200)
+                .json({ message: "User already verified", success: false });
         }
-        user.isVerified= true;
+        user.isVerified = true;
         user.verificationCode = undefined;
-        const updatedUser= await user.save();
+        const updatedUser = await user.save();
 
         return res
             .status(200)
-            .json({ message: "User verified successfully", success: true ,data:updatedUser});
+            .json({ message: "User verified successfully", success: true, data: updatedUser });
     } catch (error) {
+        await User.findByIdAndDelete(updatedUser._id);
         console.log("error while verifying user", error)
         res
             .status(500)
             .json({ message: "Error while verifying user", error, success: false });
-        
+
     }
-}   
+}
 // module.exports.
-const forgotPassword1=async(req,res)=>{
+const forgotPassword1 = async (req, res) => {
     try {
-        const {email,password}=req.body;
-        if(!email || !password){
-            throw new ApiError(400,"Email and password are required")
+        const { email, password } = req.body;
+        if (!email || !password) {
+            throw new ApiError(400, "Email and password are required")
         }
-        const user=await User.findOne({email});
-        if(!user){
-            throw new ApiError(404,"Username && email not found")
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new ApiError(404, "Username && email not found")
         }
-       await User.findOneAndUpdate({email},{password})
-        user.password=password;
+        await User.findOneAndUpdate({ email }, { password })
+        user.password = password;
         // await user.save();
         return res
-        .status(200)
-        .json({message:"Password updated successfully",success:true})
-        
+            .status(200)
+            .json({ message: "Password updated successfully", success: true })
+
     } catch (error) {
-        console.log("error in forgetting password",error)
-        throw new ApiError(500,error?.message || "Something went wrong while forgot password")
+        console.log("error in forgetting password", error)
+        throw new ApiError(500, error?.message || "Something went wrong while forgot password")
     }
 }
 // module.exports.
-const forgotPassword=async(req,res)=>{
+const forgotPassword = async (req, res) => {
     try {
-        const {email}=req.body;
-        if(!email){
-            return res.status(404).json({message:"User not Found against this email ",success:false})
+        const { email } = req.body;
+        if (!email) {
+            return res.status(404).json({ message: "User not Found against this email ", success: false })
         }
-        const user=await User.findOne({email});
-        const token=await jwt.sign({email},process.env.SECRET_KEY,{expiresIn: '30m'})
+        const user = await User.findOne({ email });
+        const token = await jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: '30m' })
         const link = `${"http://localhost:5173"}/reset-password/${token}`;
-        console.log("link:",link);
-const send_Email= sendEmail(email,"forgot",user.username,link);
-if(!send_Email){
-    return res.status(500).json({message:"Error while sending email",success:false})
-}
+        console.log("link:", link);
+        const send_Email = sendEmail(email, "forgot", user.username, link);
+        if (!send_Email) {
+            return res.status(500).json({ message: "Error while sending email", success: false })
+        }
 
         return res
-        .status(200)
-        .json({message:"Password reset email sent successfully",success:true,link})
+            .status(200)
+            .json({ message: "Password reset email sent successfully", success: true, link })
     } catch (error) {
-        console.log("error in forgeting password",error)
-        throw new ApiError(500,error?.message || "Something went wrong while forgot password")
+        console.log("error in forgeting password", error)
+        throw new ApiError(500, error?.message || "Something went wrong while forgot password")
     }
 }
 // module.exports.
-const resetPassword=async(req,res)=>{
+const resetPassword = async (req, res) => {
     try {
-        const {token,password}=req.body;
+        const { token, password } = req.body;
         // if(!token || !password){
         //     return res.status(404).json({message:"User not Fonud against this email ",success:false})
         // }
-        const decodedToken= await jwt.verify(token,process.env.SECRET_KEY);
-        console.log("token in reset password : ",token  )
-        if(!decodedToken){
-            return res.status(404).json({message:"User not Fonud against this token  ",success:false})
+        const decodedToken = await jwt.verify(token, process.env.SECRET_KEY);
+        console.log("token in reset password : ", token)
+        if (!decodedToken) {
+            return res.status(404).json({ message: "User not Fonud against this token  ", success: false })
         }
 
-        const user=await User.findOne({email:decodedToken.email});
-        if(!user){
-            return res.status(404).json({message:"User not Fonud against this email ",success:false})
+        const user = await User.findOne({ email: decodedToken.email });
+        if (!user) {
+            return res.status(404).json({ message: "User not Fonud against this email ", success: false })
         }
-        user.password=password;
-        const newUser=await user.save();
+        user.password = password;
+        const newUser = await user.save();
         return res
-        .status(200)
-        .json({message:"Password updated successfully",success:true,data:newUser})
+            .status(200)
+            .json({ message: "Password updated successfully", success: true, data: newUser })
 
 
     } catch (error) {
-        console.log("error in reset password",error)    
-        res.status(500).json({message:"Error while resetting password",error,success:false})
+        console.log("error in reset password", error)
+        res.status(500).json({ message: "Error while resetting password", error, success: false })
     }
 }
 
@@ -221,7 +240,7 @@ const generateAccessAndRefreshToken = async (userId) => {
         // console.log(user._id);
         // console.log(accessToken);
 
-        return { accessToken, refreshToken ,accessTokenExpiresIn:accessToken.expiresIn,refreshTokenExpiresIn:refreshToken.expiresIn};
+        return { accessToken, refreshToken, accessTokenExpiresIn: accessToken.expiresIn, refreshTokenExpiresIn: refreshToken.expiresIn };
 
     } catch (error) {
         console.log("error in genrating token : ", error);
@@ -244,14 +263,14 @@ const loginUser = async (req, res) => {
             $or: [{ username }, { email }]
         })
         if (!user) {
-            throw new ApiError(404,  "User not found");
+            throw new ApiError(404, "User not found");
         }
         const isPasswordValid = await user.isPasswordCorrect(password)
         if (!isPasswordValid) {
             // throw new ApiError(401,"Password Is In-Corect ");
             return res.status(401).json({ message: "Password Is In-Corect ", success: false });
         }
-        
+
         const { refreshToken, accessToken } = await generateAccessAndRefreshToken(user._id)
         const option = {
             httpOnly: true,
@@ -309,7 +328,7 @@ const logoutUser = async (req, res) => {
 
 }
 
-module.exports={
+module.exports = {
     registerUser,
     verifyUser,
     loginUser,
